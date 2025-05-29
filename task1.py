bcd054913f79de644234f3b38205aaf860ad6552
Sasanka Bhusana Jena <136557319+sasanka2904@users.noreply.github.com>
2025-05-29 10:23:21 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -3,10 +3,10 @@ import tkinter as tk
 from tkinter import ttk, messagebox
 import json
 
-class ModernTodo:
+class Todo:
     def __init__(self, master):
         self.master = master
-        self.master.title("Modern To-Do List")
+        self.master.title("To-Do List")
         self.master.geometry("400x500")
         self.master.configure(bg="#f0f0f0")
 
@@ -79,5 +79,5 @@ class ModernTodo:
 
 if __name__ == "__main__":
     root = tk.Tk()
-    app = ModernTodo(root)
+    app = Todo(root)
     root.mainloop()
